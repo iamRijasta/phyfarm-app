ebb0e2e0e4dd0932ab4ae1e4239929f12edc1ebd
Rijasta <iamrijasta@gmail.com>
2023-09-16 02:17:26 +0530

    2nd slide

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <section className={`hero`}>
-        <div className={`banner-animation-wrapper  m-24`}>
+        <div className={`banner-animation-wrapper`}>
         <div className={`banner-animation-wrapper-blur`}></div>
         </div><object type="image/svg+xml" data="/banner.svg" className={`banner-animation`}>svg-animation</object>
       </section>
